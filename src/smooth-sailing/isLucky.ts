b8e572434d632ff84c060af452cdc163cc713682
Eduard20 <esimonyan2014+github@gmail.com>
2020-07-01 21:21:18 +0400

    Create isLucky.ts

---
+++ src/smooth-sailing/isLucky.ts
@@ -0,0 +1,20 @@
+/**
+ * isLucky
+ * @param {number} n
+ * @return {boolean}
+ */
+function isLucky(n: number): boolean {
+  const nArray: string[] = n.toString().split('');
+  let firstHalf: number = 0;
+  let secondHalf: number = 0;
+
+  for (let i = 0; i < nArray.length; i++) {
+    if (i < nArray.length / 2) {
+      firstHalf = firstHalf + parseInt(nArray[i]);
+    } else {
+      secondHalf = secondHalf + parseInt(nArray[i]);
+    }
+  }
+
+  return firstHalf === secondHalf;
+}
