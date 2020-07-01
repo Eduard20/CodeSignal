a40e008ebd097daebe12ca9989dca3161b71e55d
Eduard20 <esimonyan2014+github@gmail.com>
2020-07-01 21:08:33 +0400

    Create allLongestStrings.ts

---
+++ src/smooth-sailing/allLongestStrings.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * allLongestStrings
+ * @param inputArray
+ */
+
+function allLongestStrings(inputArray: string[]): string[] {
+  const result: string[] = [""];
+  let count: number = 0;
+
+  for (let i = 0; i < inputArray.length; i++) {
+    const iLength: number = inputArray[i].length;
+    const lastElLength: number = result[result.length - 1].length;
+
+    if (iLength >= lastElLength) {
+      count = iLength > lastElLength ? 1 : ++count;
+      result.push(inputArray[i]);
+    }
+  }
+
+  return result.slice(result.length - count);
+}
