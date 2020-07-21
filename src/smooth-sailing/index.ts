a385c4a74e4eb249e364b624aa8cf4db056792c9
Eduard20 <esimonyan2014+github@gmail.com>
2020-07-21 15:08:21 +0400

    Create index.ts

---
+++ src/smooth-sailing/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Section 3: Smooth Sailing
+ */
+
+export * from './allLongestStrings';
+export * from './commonCharacterCount';
+export * from './isLucky';
+export * from './reverseInParentheses';
+export * from './sortByHeight';
