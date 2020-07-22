75f9b7fb3bd2db54050f957bb8a7d6669075c2d7
Eduard20 <esimonyan2014+github@gmail.com>
2020-07-22 18:05:09 +0400

    Create add-border.ts

---
+++ src/exploring-the-waters/add-border.ts
@@ -0,0 +1,27 @@
+/**
+ * addBorder
+ * @param {Array} picture
+ * @return {Array}
+ */
+
+function addBorder(picture: string[]): string[] {
+  const n: number = picture[0].length + 2;
+  const newPicturesList: string[] = [];
+  const border = (() => {
+    let s: string = '';
+    for (let i: number = 0; i < n; ++i) {
+      s += '*';
+    }
+    return s;
+  })();
+
+  newPicturesList.push(border);
+
+  for (let i: number = 0; i < picture.length; ++i) {
+    newPicturesList.push('*' + picture[i] + '*');
+  }
+
+  newPicturesList.push(border);
+
+  return newPicturesList;
+}
