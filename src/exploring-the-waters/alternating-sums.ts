2c7b5317ca27020fc688d14b091b52cc5cf066a5
Eduard20 <esimonyan2014+github@gmail.com>
2020-07-22 18:02:15 +0400

    Create alternating-sums.ts

---
+++ src/exploring-the-waters/alternating-sums.ts
@@ -0,0 +1,19 @@
+/**
+ * alternatingSums
+ * @param {Array} a
+ * @return {Array}
+ */
+
+function alternatingSums(a: number[]): number[] {
+  const sum: number[] = [0, 0];
+
+  for (let i = 0; i < a.length; i++) {
+    if (i % 2 == 0) {
+      sum[0] += a[i];
+    } else {
+      sum[1] += a[i];
+    }
+  }
+
+  return sum;
+}
