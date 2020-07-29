007b4f79ee7abc6aa81d990147e3a05333ba485d
Eduard20 <esimonyan2014+github@gmail.com>
2020-07-30 03:37:00 +0400

    Create are-similar.ts

---
+++ src/exploring-the-waters/are-similar.ts
@@ -0,0 +1,25 @@
+/**
+ * areSimilar
+ * @param {Array} a
+ * @param {Array} b
+ * @return Boolean
+ */
+function areSimilar(a: number[], b: number[]): boolean {
+  if (a.length !== b.length) {
+    return false;
+  }
+  const tempArr: number[] = [];
+  for (let i: number = 0; i < b.length; i++) {
+    if (a[i] !== b[i]) {
+      tempArr.push(i);
+    }
+  }
+
+  if (tempArr.length === 0) {
+    return true;
+  } else if (tempArr.length !== 2) {
+    return false;
+  }
+
+  return a[tempArr[0]] === b[tempArr[1]] && a[tempArr[1]] === b[tempArr[0]]
+}
