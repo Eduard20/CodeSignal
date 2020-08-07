a0891c0af7247fa8cc5bb47de06b0e0dae70e352
Eduard20 <esimonyan2014+github@gmail.com>
2020-08-08 02:20:06 +0400

    Updated file anme for Section 2

--- src/edge-of-the-ocean/index.ts
+++ src/edge-of-the-ocean/index.ts
@@ -2,8 +2,8 @@
  * Section 2: Edge Of The Ocean
  */
 
-export * from './adjacentElementsProduct';
-export * from './almostIncreasingSequence';
-export * from './makeArrayConsecutive2';
-export * from './matrixElementsSum';
-export * from './shapeArea';
+export * from 'edge-of-the-ocean/adjacent-elements-product';
+export * from 'edge-of-the-ocean/almost-increasing-sequence';
+export * from 'edge-of-the-ocean/make-array-consecutive2';
+export * from 'edge-of-the-ocean/matrix-elements-sum';
+export * from 'edge-of-the-ocean/shape-area';
