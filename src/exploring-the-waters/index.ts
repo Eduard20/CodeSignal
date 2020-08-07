38132965229658c8d25f65bf878c701cbd66cdc2
Eduard20 <esimonyan2014+github@gmail.com>
2020-08-08 02:17:13 +0400

    Create index.ts

---
+++ src/exploring-the-waters/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Section 4: Exploring The Waters
+ */
+
+export * from './add-border';
+export * from './alternating-sums';
+export * from './are-similar';
+export * from './array-change';
+export * from './palindrome-rearranging';
