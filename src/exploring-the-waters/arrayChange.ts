f6642cd1d844b46b791723b44ba789f350ddb127
Eduard20 <esimonyan2014+github@gmail.com>
2020-08-07 17:46:28 +0400

    Create arrayChange.ts

---
+++ src/exploring-the-waters/arrayChange.ts
@@ -0,0 +1,12 @@
+function arrayChange(inputArray: number[]): number {
+  let moves: number = 0;
+
+  for (let i: number = 0; i < inputArray.length; i++) {
+    if (inputArray[i] <= inputArray[i - 1]) {
+      moves += inputArray[i - 1] - inputArray[i] + 1;
+      inputArray[i] = inputArray[i - 1] + 1;
+    }
+  }
+
+  return moves;
+}
