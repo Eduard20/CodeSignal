ee2dfaaeff5cfc8bdd9f83c60629db7e7b873656
Eduard20 <esimonyan2014+github@gmail.com>
2020-08-08 02:14:27 +0400

    Create palindrome-rearranging.ts

---
+++ src/exploring-the-waters/palindrome-rearranging.ts
@@ -0,0 +1,24 @@
+function palindromeRearranging(inputString: string): boolean {
+  let strObj: any = {};
+  if (inputString.length === 1) {
+    return true;
+  }
+  const inputToArr: string[] = inputString.split('');
+
+  for (let i: number = 0; i < inputToArr.length; i++) {
+    if (!strObj.hasOwnProperty(inputToArr[i])) {
+      strObj[inputToArr[i]] = 1;
+      continue;
+    }
+    strObj[inputToArr[i]]++
+  }
+  let oddCount: number = 0;
+  for (let prop in strObj) {
+
+    if (strObj[prop] % 2 === 1) {
+      oddCount++
+    }
+  }
+
+  return oddCount < 2;
+}
