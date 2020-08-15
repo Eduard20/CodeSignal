2fb5d38857fe2957d3bb5f0bab95e12522e6d603
Eduard20 <esimonyan2014+github@gmail.com>
2020-08-15 16:39:55 +0400

    Update is-IPv4-address.ts

--- src/island-of-knowledge/is-IPv4-address.ts
+++ src/island-of-knowledge/is-IPv4-address.ts
@@ -4,6 +4,6 @@
  * @return {Boolean}
  */
 
-function isIPv4Address(inputString: string): boolean {
+export function isIPv4Address(inputString: string): boolean {
   return /^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$/.test(inputString);
 }
