9ddc71adfb6c590dd7c276f22cca323f610571c5
Eduard20 <esimonyan2014+github@gmail.com>
2020-10-13 00:31:27 +0400

    Update array-replace.ts

--- src/rains-of-reason/array-replace.ts
+++ src/rains-of-reason/array-replace.ts
@@ -4,6 +4,7 @@
  * @param {Number} elemToReplace
  * @param {Number} substitutionElem
  */
+
 function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
   for (let index: number = 0; index < inputArray.length; index++) {
     if (inputArray[index] === elemToReplace) {
