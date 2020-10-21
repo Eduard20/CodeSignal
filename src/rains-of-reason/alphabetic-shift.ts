11a674a3be918b6a35e9faad62815fc2fcbbb903
Eduard20 <esimonyan2014+github@gmail.com>
2020-10-21 14:33:18 +0400

    Create alphabetic-shift.ts

---
+++ src/rains-of-reason/alphabetic-shift.ts
@@ -0,0 +1,11 @@
+function alphabeticShift(inputString: string): string {
+  let outputString: string = '';
+
+  for (let i: number = 0; i < inputString.length; ++i) {
+    const code: number = inputString.charCodeAt(i) % 122;
+
+    outputString += (code == 0 ? 'a' : String.fromCharCode(code + 1));
+  }
+
+  return outputString;
+}
